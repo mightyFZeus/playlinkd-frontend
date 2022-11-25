e2d6c0af508b8812232d1720e9a1fed4af6fdcb8
Solace4415 <tijjtay@gmail.com>
2022-11-25 21:36:47 +0100

    changes made

--- src/HOC/Wrapper.tsx
+++ src/HOC/Wrapper.tsx
@@ -6,7 +6,7 @@ const Wrapper = ({ children }: PropsWithChildren) => {
       <div className="absolute bottom-0 w-full">
         <img src="/assets/Vector 2.png" alt="" className="w-full h-[500px] md:h-[600px] lg:h-[400px] object-fit" />
       </div>
-      <div className="absolute bottom-0 h-[520px] md:h-[600px] lg:top-0 lg:h-full md:right-[100px]">
+      <div className="absolute bottom-0 h-[350px] md:h-[600px] lg:top-0 lg:h-full md:right-[100px]">
         <img src="/assets/Vector 5.png" alt="" className=" h-full object-cover md:object-fit" />
       </div>
       {children}
